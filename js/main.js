de88b571fd94cc59af96c54def18e698b8febcc3
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-05-27 03:11:29 +0500

    initial commit

--- js/main.js
+++ js/main.js
@@ -16,3 +16,42 @@ Navigatsion.addEventListener("animationend", function (evt) {
     Navigatsion.classList.remove("header__nav--closing");
   }
 });
+
+$(document).ready(function () {
+  $("#submit").click(function () {
+    var name = $("#name").val();
+    var number = $("#tel").val();
+    var password = $("#password").val();
+    var password_2 = $("#password_2").val();
+    var bio = $("#bio").val();
+    var email = $("#email").val();
+    if (name == "") {
+      alert("Введите ваше имя");
+    } else if (tel == "") {
+      alert("Введите номер");
+    } else if (email == "") {
+      alert("Введите ваш E-mail");
+    } else if (password == "") {
+      alert("Введите ваш пароль");
+    } else if (password_2 == "") {
+      alert("Повторите пароль");
+    } else if (password != password_2) {
+      alert("Введенные пароли не совпадают");
+    } else if (password.lenght < 4) {
+      alert("Пароль должен быть не меньше 4 символов");
+    } else {
+      alert(
+        "Имя:  " +
+          name +
+          "\n Номер тел:  " +
+          tel +
+          "\n E-mail:  " +
+          email +
+          "\n Пароль:  " +
+          password +
+          "\n О себе:  " +
+          bio
+      );
+    }
+  });
+});
